efcd90cf6ecc8e79a9409f22da4431be03add936
Shaheer cp <shaheercp890@gmail.com>
2025-02-19 11:59:38 +0530

    TS variable declaration and initialization

---
+++ script.ts
@@ -0,0 +1,12 @@
+// Variable declaration and initialization
+
+let userName : string = "shaheer";
+let age : number = 22;
+let alive : boolean = true;
+let skills : (string | number) []= ['JS','Node',34];
+let emptyArr : [] = [];
+let userDetail : {name:string;age:number ; salary:number} = {
+    name :'shaheer',
+    age:22,
+    salary:1000000
+}
